6a301bab5313a61f80de25acb6b73d8f276f16a5
Martina Anene-Ogbe <cmartinaanene@gmail.com>
2025-09-26 19:32:54 +0100

    Update 02_mriqc.sh

--- scripts/protocol_specific/02_mriqc.sh
+++ scripts/protocol_specific/02_mriqc.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-# Script 02: MRIQC
-# Run MRIQC on the BIDS dataset
+# MRIQC pipeline (protocol-specific version)
 
-mriqc ./bids_dataset ./derivatives/mriqc participant
+# Step 1: Visual Inspection of Structural Images
+ml fsl/6.0.7.8
+
+# Navigate to anatomical folder
+cd ~/Desktop/CAPSTONE/capstonebids/sub-02/anat
+
+# Open the T1w MRI image in FSLeyes
+fsleyes sub-02_T1w.nii.gz
+
+# Optional: Open PET image for visual inspection
+cd ~/Desktop/CAPSTONE/capstonebids/sub-02/pet
+fsleyes sub-02_pet.nii.gz
+
+# Step 2: Generate Automated QC Metrics with MRIQC
+ml mriqc/v1.0.20240202
+
+# Set paths
+export BIDSDIR=~/Desktop/CAPSTONE/capstonebids/
+mkdir -p ~/Desktop/derivatives/
+export MRIQCDIR=~/Desktop/derivatives/
+
+# Run participant-level QC
+mriqc $BIDSDIR $MRIQCDIR participant
+
+# Step 3: View MRIQC Results
+cd $MRIQCDIR
+open sub-02_T1w.html
