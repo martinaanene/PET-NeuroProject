0518800237cebbc3a97a22380c2537ffd38aac1c
Toluene-AI <atadeniyi@student.oauife.edu.ng>
2025-10-02 21:16:56 +0100

    Update 03_preprocessing.sh

--- scripts/protocol_specific/03_preprocessing.sh
+++ scripts/protocol_specific/03_preprocessing.sh
@@ -5,18 +5,18 @@
 # Step 1: Visual Reorientation
 ml fsl/6.0.7.8
 
-cd ~/Desktop/CAPSTONE/sub-02/anat
+cd ~/Desktop/CAPSTONE/sub-02/anat/
 fslreorient2std sub-02_T1w.nii.gz
 
-cd ~/Desktop/CAPSTONE/sub-02/pet
+cd ~/Desktop/CAPSTONE/sub-02/pet/
 fslreorient2std sub-02_pet.nii.gz
 
 # Step 2: Co-registration of PET and MRI
-cd ~/Desktop/CAPSTONE/sub-02/anat
+cd ~/Desktop/CAPSTONE/sub-02/anat/
 bet sub-02_T1w.nii.gz sub-02_T1w_brain.nii.gz -R -f 0.2 -m
 fsleyes sub-02_T1w.nii.gz sub-02_T1w_brain.nii.gz -cm red
 
-cd ~/Desktop/CAPSTONE/sub-02
+cd ~/Desktop/CAPSTONE/sub-02/
 flirt -in pet/sub-02_pet.nii.gz \
       -ref anat/sub-02_T1w_brain.nii.gz \
       -out pet/sub-02_pet_coreg.nii.gz \
@@ -33,7 +33,7 @@ flirt -in anat/sub-02_T1w_brain.nii.gz \
       -omat anat/sub-02_T1w_to_MNI_linear.mat \
       -out anat/sub-02_T1w_to_MNI_linear.nii.gz
 
-fnirt --in=anat/sub-02_T1w.nii.gz \
+fnirt --in=anat/sub-02_T1w_brain.nii.gz \
       --ref=$FSLDIR/data/standard/MNI152_T1_1mm.nii.gz \
       --aff=anat/sub-02_T1w_to_MNI_linear.mat \
       --cout=anat/sub-02_T1w_to_MNI_warp.nii.gz \
@@ -44,10 +44,11 @@ fnirt --in=anat/sub-02_T1w.nii.gz \
 applywarp --in=pet/sub-02_pet.nii.gz \
           --ref=$MNI_TEMPLATE_BRAIN \
           --warp=anat/sub-02_T1w_to_MNI_warp.nii.gz \
-          --out=pet/sub-02_pet_MNI.nii.gz
+          --premat=pet/sub-02_pet_to_T1w.mat \
+          --out=pet/sub-02_pet_to_MNI.nii.gz
 
 # Step 4: Smoothing
-fslmaths pet/sub-02_pet_MNI.nii.gz -s 3.4 pet/sub-064_ses-03_pet_MNI_smoothed.nii.gz
+fslmaths pet/sub-02_pet_to_MNI.nii.gz -s 3.4 pet/sub-02_pet_to_MNI_smoothed.nii.gz
 
 # Step 5: SUVR Calculation
-fslstats sub-02_pet_MNI.nii.gz -k cerebellum_mask.nii.gz -M
+fslstats sub-02_pet_to_MNI.nii.gz -k cerebellum_mask.nii.gz -M
